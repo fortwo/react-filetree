0d3c46d63c776820479ba0ce1d57825881de00ae
Francesco Fortunati <fortunati.francesco89@gmail.com>
2017-05-20 00:14:53 +0200

    feature(): workbasic inrking server

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+	<div>
+	    <h1>React File Tree</h1>
+  	</div>,
+  	document.getElementById('root'),
+);
---
+++ webpack.config.babel.js
@@ -0,0 +1,21 @@
+import path from 'path';
+import webpack from 'webpack';
+ 
+const options = {
+  entry: './index.js',
+  output: { path: __dirname, filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
+
+module.exports = options;
