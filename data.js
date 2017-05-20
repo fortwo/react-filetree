a309a89f3424b605b45952e7b1abed7af3325442
Francesco Fortunati <fortunati.francesco89@gmail.com>
2017-05-20 15:50:10 +0200

    refactor(): remove icons from sample data

--- data.js
+++ data.js
@@ -2,46 +2,37 @@ const data = [
 	{
 		id: '0',
 		name: 'Folder 1',
-		icon: 'fa',
 		children: [
 			{
 				id: '1',
 				name: 'File 1 in Folder 1',
-				icon: 'fa',
 			}, {
 				id: '2',
 				name: 'File 2 in Folder 2',
-				icon: 'fa',
 			},
 		],
 	}, {
 		id: '3',
 		name: 'Empty Folder',
-		icon: 'fa',
 		children: [],
 	}, {
 		id: '4',
 		name: 'Folder 2',
-		icon: 'fa',
 		children: [
 			{
 				id: '5',
 				name: 'File 1 in Folder 2',
-				icon: 'fa',
 			}, {
 				id: '6',
 				name: 'File 2 in Folder 2',
-				icon: 'fa',
 			}, {
 				id: '7',
 				name: 'File 3 in Folder 2',
-				icon: 'fa',
 			},
 		],
 	}, {
 		id: '8',
 		name: 'File in root',
-		icon: 'fa',
 	},
 ];
 
