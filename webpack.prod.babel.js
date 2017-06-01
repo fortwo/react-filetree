3b1aee3a07d4e98a93093717f3df183ec71497dc
Francesco Fortunati <fortunati.francesco89@gmail.com>
2017-06-01 08:04:35 +0200

    feature(): build configuration for production

---
+++ webpack.prod.babel.js
@@ -0,0 +1,35 @@
+import path from 'path';
+
+const options = {
+	entry: './src/FileTree.js',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'react-filetree.js',
+		publicPath: '/dist',
+	},
+	module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }, {
+        test: /\.(jpg|png|gif)$/,
+        loader: 'file-loader',
+      }, {
+        test: /\.(jpg|png|gif)$/,
+        loader: 'image-webpack-loader',
+        query: {
+          optipng: {
+            optimizationLevel: 7,
+          },
+        }
+      },
+    ]
+  },
+};
+
+module.exports = options;
